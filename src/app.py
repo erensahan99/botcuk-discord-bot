92f32002db2a6a11def954056f04416be42ad7a7
Eren <eren.sahan99@gmail.com>
2021-01-24 23:47:00 +0300

    Added new commands

--- src/app.py
+++ src/app.py
@@ -3,9 +3,9 @@ import os
 import discord
 from discord.ext import commands
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
 
@@ -16,6 +16,7 @@ async def on_ready():
     print("I'm ready!!!")
     await client.change_presence(status = discord.Status.online, activity = discord.Game("Naparsınız uşaklar😋"))
 
+'''
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -23,9 +24,19 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+'''
 
 @client.command(name="ping")
-async def ping(ctx) :
+async def ping(ctx):
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+
+@client.command(name="deneme")
+async def deneme(ctx):
+    await ctx.send(f"{ ctx.message.content }")
+    
+@client.command(name="calc")
+async def deneme(ctx):
+    msg = ctx.message.content.split(' ', 1)[1]
+    await ctx.send(f"{ eval(msg) }")
 
 client.run(token)
